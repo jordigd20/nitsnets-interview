626fb4e9de2ad440520c49a95583618962460981
jordigd20 <jordigd8@gmail.com>
2024-11-12 18:14:18 +0100

    chore: Update Prettier and ESLint configurations for improved Vue support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,18 @@
 import withNuxt from './.nuxt/eslint.config.mjs';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-export default withNuxt(eslintPluginPrettierRecommended);
+export default withNuxt(
+  [
+    {
+      rules: {
+        'prettier/prettier': [
+          'error',
+          {
+            vueIndentScriptAndStyle: true
+          }
+        ]
+      }
+    }
+  ],
+  eslintPluginPrettierRecommended
+);
