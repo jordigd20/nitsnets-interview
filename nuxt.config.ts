17c5526bfd0bc5f6e2ec02e2820b6054e3093b9f
jordigd20 <jordigd8@gmail.com>
2024-11-12 13:56:40 +0100

    feat: Integrate Vuetify with Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,33 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  build: {
+    transpile: ['vuetify']
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        config.plugins?.push(vuetify({ autoImport: true }));
+      });
+    },
+    '@nuxt/eslint'
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls
+      }
+    }
+  },
+  app: {
+    head: {
+      title: 'Nitsnets - Prueba técnica',
+      htmlAttrs: {
+        lang: 'es'
+      }
+    }
+  }
 });
