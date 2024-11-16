1490d2873a0166ab5965bc7e5dae5c9d0f10f1dc
jordigd20 <jordigd8@gmail.com>
2024-11-16 12:23:09 +0100

    feat: Add useAuthStore for user authentication and session management

---
+++ stores/auth.ts
@@ -0,0 +1,49 @@
+import type { User, AuthResponse } from '~/interfaces';
+
+export const useAuthStore = defineStore('auth', () => {
+  const user = ref<User | null>(null);
+  const token = ref<string | null>(null);
+  const isLoading = ref(false);
+  const error = ref<string | null>(null);
+
+  const isAuthenticated = computed(() => !!token.value);
+
+  const login = async (email: string, password: string) => {
+    try {
+      isLoading.value = true;
+      const response = await $fetch<AuthResponse>(`${BASE_API_URL}/login`, {
+        method: 'POST',
+        body: {
+          email,
+          password
+        }
+      });
+
+      user.value = response.user;
+      token.value = response.token;
+
+      const tokenCookie = useCookie('token');
+      tokenCookie.value = response.token;
+      error.value = null;
+      await navigateTo('/dashboard');
+    } catch (err) {
+      console.error(err);
+      error.value = 'Error al iniciar sesión.';
+    }
+
+    isLoading.value = false;
+  };
+
+  const setAuthData = (data: AuthResponse) => {
+    user.value = data.user;
+    token.value = data.token;
+  };
+
+  const logout = () => {
+    user.value = null;
+    token.value = null;
+    useCookie('token').value = null;
+  };
+
+  return { user, token, isLoading, error, isAuthenticated, login, logout, setAuthData };
+});
---
+++ utils/constants.ts
@@ -0,0 +1,1 @@
+export const BASE_API_URL = 'https://fc692e31-74e2-4167-b8a5-76d61603bff9.mock.pstmn.io';
