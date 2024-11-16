7d28a40fcaf75d9560f2cf5fa573703280543ae2
jordigd20 <jordigd8@gmail.com>
2024-11-16 14:37:58 +0100

    refactor: Update register page to have a separate RegisterForm component

---
+++ components/RegisterForm.vue
@@ -0,0 +1,91 @@
+<script setup lang="ts">
+  type Props = {
+    isLoading: boolean;
+    alert: {
+      display: boolean;
+      type: 'error' | 'success' | 'info' | 'warning';
+      message: string;
+    };
+  };
+  const { alert, isLoading } = defineProps<Props>();
+  const emit = defineEmits<{
+    submit: [
+      {
+        valid: boolean;
+        name: string;
+        email: string;
+        nif: string;
+        password: string;
+        confirmPassword: string;
+      }
+    ];
+  }>();
+
+  const { email, emailRules } = useEmail();
+  const { password, passwordRules } = usePassword();
+  const { nif, nifRules } = useNif();
+  const { confirmPassword, confirmPasswordRules } = useConfirmPassword(password);
+  const name = ref('');
+  const valid = ref(false);
+
+  const rules = {
+    email: emailRules,
+    nif: nifRules,
+    password: passwordRules,
+    confirmPassword: confirmPasswordRules
+  };
+</script>
+
+<template>
+  <v-form
+    v-model="valid"
+    class="auth-form"
+    @submit.prevent="() => emit('submit', { valid, name, email, nif, password, confirmPassword })"
+  >
+    <v-text-field v-model.trim="name" label="Nombre" type="text" variant="outlined"></v-text-field>
+
+    <v-text-field
+      v-model.trim="email"
+      label="* Correo electrónico"
+      type="email"
+      :rules="rules.email"
+      required
+      variant="outlined"
+    ></v-text-field>
+
+    <v-text-field
+      v-model.trim="nif"
+      label="* NIF"
+      type="text"
+      :rules="rules.nif"
+      required
+      variant="outlined"
+    ></v-text-field>
+
+    <v-text-field
+      v-model.trim="password"
+      label="* Contraseña"
+      type="password"
+      :rules="rules.password"
+      required
+      variant="outlined"
+    ></v-text-field>
+
+    <v-text-field
+      v-model.trim="confirmPassword"
+      label="* Confirmar contraseña"
+      type="password"
+      :rules="rules.confirmPassword"
+      required
+      variant="outlined"
+    ></v-text-field>
+
+    <p class="auth-reminder">Los campos marcados con un (*) son obligatorios.</p>
+
+    <v-alert v-if="alert.display" class="mb-4" :type="alert.type" variant="tonal" density="compact">
+      {{ alert.message }}
+    </v-alert>
+
+    <v-btn type="submit" color="primary" :loading="isLoading" block> Crear cuenta </v-btn>
+  </v-form>
+</template>
--- pages/register.vue
+++ pages/register.vue
@@ -1,44 +1,57 @@
 <script setup lang="ts">
-  import { useEmail, usePassword, useNif, useConfirmPassword } from '~/composables';
   import type { User } from '~/interfaces/user.interface';
 
-  const { email, emailRules } = useEmail();
-  const { password, passwordRules } = usePassword();
-  const { nif, nifRules } = useNif();
-  const { confirmPassword, confirmPasswordRules } = useConfirmPassword(password);
-  const name = ref('');
-  const isLoading = ref(false);
-  const error = ref('');
-  const valid = ref(false);
-
-  const rules = {
-    email: emailRules,
-    nif: nifRules,
-    password: passwordRules,
-    confirmPassword: confirmPasswordRules
+  type Alert = {
+    display: boolean;
+    type: 'error' | 'success' | 'info' | 'warning';
+    message: string;
   };
 
-  const handleSubmit = async () => {
-    if (!valid.value) return;
-    if (password.value !== confirmPassword.value) return;
+  const isLoading = ref(false);
+  const alert = ref<Alert>({
+    display: false,
+    type: 'error',
+    message: ''
+  });
+
+  const handleSubmit = async (data: {
+    valid: boolean;
+    name: string;
+    email: string;
+    nif: string;
+    password: string;
+    confirmPassword: string;
+  }) => {
+    if (!data.valid) return;
+    if (data.password !== data.confirmPassword) return;
 
     isLoading.value = true;
 
     try {
-      const response = await useFetchData<User>('/register', 'POST', {
-        name: name.value,
-        email: email.value,
-        nif: nif.value,
-        password: password.value
+      const response = await $fetch<User>(`${BASE_API_URL}/register`, {
+        method: 'POST',
+        body: {
+          name: data.name,
+          email: data.email,
+          nif: data.nif,
+          password: data.password
+        }
       });
 
       if (!response) throw new Error('Error al crear la cuenta.');
 
-      console.log(response);
-      await navigateTo('/');
+      alert.value = {
+        display: true,
+        type: 'success',
+        message: 'Cuenta creada correctamente.'
+      };
     } catch (err) {
       console.error(err);
-      error.value = 'Error al crear la cuenta.';
+      alert.value = {
+        display: true,
+        type: 'error',
+        message: 'Error al crear la cuenta.'
+      };
     }
 
     isLoading.value = false;
@@ -49,61 +62,7 @@
   <NuxtLayout name="auth">
     <h1>Crear cuenta</h1>
 
-    <v-form v-model="valid" class="auth-form" @submit.prevent="handleSubmit">
-      <v-text-field
-        v-model.trim="name"
-        label="Nombre"
-        type="text"
-        variant="outlined"
-      ></v-text-field>
-
-      <v-text-field
-        v-model.trim="email"
-        label="* Correo electrónico"
-        type="email"
-        :rules="rules.email"
-        required
-        variant="outlined"
-      ></v-text-field>
-
-      <v-text-field
-        v-model.trim="nif"
-        label="* NIF"
-        type="text"
-        :rules="rules.nif"
-        required
-        variant="outlined"
-      ></v-text-field>
-
-      <v-text-field
-        v-model.trim="password"
-        label="* Contraseña"
-        type="password"
-        :rules="rules.password"
-        required
-        variant="outlined"
-      ></v-text-field>
-
-      <v-text-field
-        v-model.trim="confirmPassword"
-        label="* Confirmar contraseña"
-        type="password"
-        :rules="rules.confirmPassword"
-        required
-        variant="outlined"
-      ></v-text-field>
-
-      <p class="auth-reminder">Los campos marcados con un (*) son obligatorios.</p>
-
-      <p
-        v-if="error"
-        class="alert-error pa-3 text-body-2 border-sm border-error border-opacity-100 rounded mb-4"
-      >
-        {{ error }}
-      </p>
-
-      <v-btn type="submit" color="primary" :loading="isLoading" block> Crear cuenta </v-btn>
-    </v-form>
+    <RegisterForm :alert="alert" :is-loading="isLoading" @submit="handleSubmit" />
 
     <footer class="auth-footer">
       <NuxtLink to="/"> Iniciar sesión </NuxtLink>
