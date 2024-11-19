2663ffed8345daf56ed0a125d80fd55fc6ea2132
jordigd20 <jordigd8@gmail.com>
2024-11-19 12:58:48 +0100

    feat: Add categories constant for product classification

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,1 +1,12 @@
 export const BASE_API_URL = 'https://fc692e31-74e2-4167-b8a5-76d61603bff9.mock.pstmn.io';
+
+export const CATEGORIES = [
+  'Sudaderas',
+  'Camisetas',
+  'Camisas',
+  'Chaquetas',
+  'Vaqueros',
+  'Pantalones de chándal',
+  'Pantalones cortos',
+  'Ropa interior'
+] as const;
