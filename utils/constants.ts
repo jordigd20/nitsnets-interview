6d63925064872f6059bf67ed48ee9ebda8a868bf
jordigd20 <jordigd8@gmail.com>
2024-11-20 20:57:34 +0100

    feat: Add more clothing categories

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,12 +1,20 @@
 export const BASE_API_URL = 'https://fc692e31-74e2-4167-b8a5-76d61603bff9.mock.pstmn.io';
 
 export const CATEGORIES = [
-  'Sudaderas',
   'Camisetas',
+  'Polos',
   'Camisas',
+  'Vestidos',
+  'Sudaderas',
   'Chaquetas',
+  'Abrigos',
+  'Jerseys',
   'Vaqueros',
   'Pantalones de chándal',
+  'Pantalones chinos',
   'Pantalones cortos',
+  'Faldas',
+  'Monos',
+  'Ropa de baño',
   'Ropa interior'
 ] as const;
